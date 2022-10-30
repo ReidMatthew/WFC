e661a84b42380a3c072f343bdbcf2ed0d3e66998
ReidMatthew <msreid92@gmail.com>
2022-10-29 20:33:35 -0400

    thing

--- hello.go
+++ hello.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type tile struct {
+	border [][]int // side patern clockwise N:0 E:1 S:2 W:3
+}
+
+type cell struct {
+	border    [][]int // side patern clockwise N:0 E:1 S:2 W:3
+	pix       int     // pixilation of edge
+	collapsed bool    // solved
+}
+
+// func newPerson(name string) *person {
+
+// 	p := person{name: name}
+// 	p.age = 42
+// 	return &p
+// }
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("a")
 }
